541f025927d7e80dbe14b960f480f462d38f7c9b
Mathieu Darrigade <mathieu.darrigade@clevy.io>
2021-04-22 13:52:51 +0200

    vla bla

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const { BotsService } = require('./bots.service');
 
 (async () => {
   try {
+    const payload = github.context.payload;
+    if (payload) console.log('this is the payload: ', payload);
+    else console.log('no payload, github context: ', github.context);
     if (process.env.INPUT_SAVE) await BotsService.saveBot();
     if (process.env.INPUT_BUILD) await BotsService.buildBot();
     if (process.env.INPUT_CREATE_LABEL) {
-      const payload = JSON.stringify(github.context.payload, undefined, 2);
+      const payload = github.context.payload;
       const label_name = payload.ref.replace('refs/tags/', '');
       await BotsService.createLabel(label_name);
     }
     if (process.env.INPUT_DELETE_LABEL) {
-      const payload = JSON.stringify(github.context.payload, undefined, 2);
+      const payload = github.context.payload;
       const label_name = payload.ref.replace('refs/tags/', '');
       await BotsService.deleteLabel(label_name);
     }
