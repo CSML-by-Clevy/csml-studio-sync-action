75de84dfc2b3a07d2c642f8a2bdce0e2f0c176ca
Mathieu Darrigade <mathieu.darrigade@clevy.io>
2021-04-22 13:23:10 +0200

    typo

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { BotService } = require('./bots.service');
+const { BotsService } = require('./bots.service');
 
 (async () => {
   try {
-    // Get the JSON webhook payload for the event that triggered the workflow.
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-
     // Retrieve the params passed through the workflow yml file.
-    if (process.env.INPUT_SAVE) await BotService.saveBot();
-    if (process.env.INPUT_BUILD) await BotService.buildBot();
+    const tmp = {
+      save: process.env.INPUT_SAVE,
+      build: process.env.INPUT_BUILD,
+      cl: process.env.INPUT_CREATE_LABEL,
+      dl: process.env.INPUT_DELETE_LABEL,
+    };
+
+    console.log(tmp);
+
+    if (process.env.INPUT_SAVE) await BotsService.saveBot();
+    if (process.env.INPUT_BUILD) await BotsService.buildBot();
     if (process.env.INPUT_CREATE_LABEL) {
       const payload = JSON.stringify(github.context.payload, undefined, 2);
       const label_name = payload.ref.replace('refs/tags/', '');
-      await BotService.createLabel(label_name);
+      await BotsService.createLabel(label_name);
     }
     if (process.env.INPUT_DELETE_LABEL) {
       const payload = JSON.stringify(github.context.payload, undefined, 2);
       const label_name = payload.ref.replace('refs/tags/', '');
-      await BotService.deleteLabel(label_name);
+      await BotsService.deleteLabel(label_name);
     }
   }
   catch (err) {
